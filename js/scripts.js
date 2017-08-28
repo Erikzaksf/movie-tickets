8c71cdec572db656b28bc529567b1dce8eb83de9
erikzaksf <erik.zak.sf@gmail.com>
2017-08-28 12:59:51 -0700

    add signUp and logIn forms to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,23 @@ $(document).ready(function() {
   $('body').css('height' , $(window).innerHeight());
   $('.wrapper').css('height' , $('body').height()- $('.jumbotron').innerHeight());
 })
+
+
+var retrievedcredentials = [];
+var credentials = {
+  username: "",
+  password: "",
+  userType: "",
+}
+
+
+localStorage.setItem('credentials', JSON.stringify(credentials));
+
+$(document).ready(function(){
+  $("form#signUp").submit(function(event){
+    event.preventDefault();
+    var name = $('#newName').val();
+    var password = $('#newPassword').val();
+    var userType = $('#userType').val();
+  });
+});
